bad435c233e5d2e53b09f8aa675f895ca0691ce0
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2016-08-29 22:12:29 -0300

    Agregado código de Ramiro + mi propio script de filtrado

---
+++ data_filter.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as mpl
+import pickle
+import filtrado
+
+datasets_path = "/Users/vale/Facultad/Tesis/data/datasets/"
+
+with open(datasets_path + "detalles_subtitulos.p", 'rb') as f:
+  subtitles_index = pickle.load(f, encoding='latin-1')
+
+
+filtered_index = subtitles_index[subtitles_index.Country.str.contains("USA", na=False)]
+filtered_index = filtered_index[filtered_index.Language.str.contains("English", na=False)]
+filtered_index = filtered_index[filtered_index.SubSumCD == 1]
+filtered_index = filtered_index[filtered_index.Type.isin(['movie','episode'])]
+filtered_index = filtrado.best_subs(filtered_index)
+
+filtered_index.to_pickle(datasets_path + "filtered_index.p")
---
+++ filtrado.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+
+from Subtitle import Subtitle
+import pandas as pd
+import numpy as np
+import random
+
+def best_subs(subs_df, seed = 1234):
+	""" Función que elige los subtitulos por pelicula de usuario
+	    de mayor ranking """
+
+	random.seed(seed)
+
+	elegidos = set()
+	UserRank = ["super admin", "administrator", "subtranslator",
+				 "platinum member", "vip plus member", "vip member",
+				 "gold member", "trusted", "silver member",
+				 "bronze member", "sub leecher", None] # Fuente: http://forum.opensubtitles.org/viewtopic.php?t=1991
+
+	# Veo las peliculas y para cada una elijo al azar un subtitulos
+	# de los usuarios con mayor ranking
+	for m in subs_df.MovieID.unique():
+		subs_df_tmp_1 = subs_df[subs_df.MovieID == m]
+		assert subs_df_tmp_1.shape[0] > 0
+		for st in UserRank:
+			# Selecciono los subtitulos del ranking que busco
+			if st == None:
+				subs_df_tmp_2 = subs_df_tmp_1[subs_df_tmp_1.UserRank.isnull()]
+			else:
+				subs_df_tmp_2 = subs_df_tmp_1[subs_df_tmp_1.UserRank==st]
+			# Si hay del ranking, elijo uno al azar
+			if subs_df_tmp_2.shape[0] > 0:
+				elegidos.add(int(random.choice(list(subs_df_tmp_2.IDSubtitleFile))))
+				break
+
+	# Armo los índices
+	ix = [True if int(e) in elegidos else False for e in subs_df.IDSubtitleFile.tolist()]
+	return subs_df[ix]
