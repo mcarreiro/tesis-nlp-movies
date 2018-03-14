9af961ac6e32a3756003d37c8918c00ae09f13a7
Martin Carreiro <martin.carreiro@mercadolibre.com>
2018-03-14 12:33:10 -0300

    requirements

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-subtitles_path = "/home/ramiro/Documents/biases_in_hollywood/data/datasets/subtitulos_popular/"
-datasets_path = "/home/ramiro/Documents/biases_in_hollywood/data/datasets/"
-wordsim_path = "/home/ramiro/Documents/biases_in_hollywood/data/wordsim353/"
+subtitles_path = "/Users/mcarreiro/Downloads/biases_in_hollywood/data/datasets/subtitulos_popular/"
+datasets_path = "/Users/mcarreiro/Downloads/biases_in_hollywood/data/datasets/"
+wordsim_path = "/Users/mcarreiro/Downloads/biases_in_hollywood/data/wordsim353/"
